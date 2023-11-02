150dddd3a184583bf42242c345a780ddd72cbdca
KG-korepetycje <glowicz10@gmail.com>
2023-11-02 14:07:52 +0100

    Setup part 2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,11 @@ android {
 }
 
 dependencies {
-
+    val lifecycle_version = "2.6.2"
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
