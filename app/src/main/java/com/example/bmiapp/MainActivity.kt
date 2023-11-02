40e476f044f219dbf1834a2dffd828f02af403c1
KG-korepetycje <glowicz10@gmail.com>
2023-11-02 22:10:05 +0100

    Testing basic functionality.

--- app/src/main/java/com/example/bmiapp/MainActivity.kt
+++ app/src/main/java/com/example/bmiapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.bmiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.calculateButton)
+        button.setOnClickListener{
+            val resultTV = findViewById<TextView>(R.id.bmiTV)
+            resultTV.text = "RESULT: DUPA"
+        }
     }
 }
