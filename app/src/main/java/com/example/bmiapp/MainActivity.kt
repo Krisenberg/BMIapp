fc623047ad44aa6e5eacc2c9a126b179ac83675a
KG-korepetycje <glowicz10@gmail.com>
2023-11-03 17:42:28 +0100

    Menu seems to be looking good with a MaterialToolbar.

--- app/src/main/java/com/example/bmiapp/MainActivity.kt
+++ app/src/main/java/com/example/bmiapp/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.bmiapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        //setSupportActionBar(findViewById(R.id.toolbar))
 
         val button = findViewById<Button>(R.id.calculateButton)
         button.setOnClickListener{
@@ -22,5 +25,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.nav_menu,menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_history -> Toast.makeText(this, "Twoja stara", Toast.LENGTH_SHORT).show()
+            R.id.nav_settings -> startActivity(Intent(this, Settings::class.java))
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
